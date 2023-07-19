b71b54d207a02f4d3e2a77d0b770255589648216
HikaruKawano <108588173+HikaruKawano@users.noreply.github.com>
2023-07-19 10:35:28 -0300

    portfolio v1

--- src/App.tsx
+++ src/App.tsx
@@ -11,46 +11,52 @@ function App() {
   return (
     <>
       <Container className="container box-border h-auto flex justify-between pb-20 my-9  rounded-2xl flex-col w-11/12 bg-zinc-800 bg-opacity-80  ">
-        <header className="w-full h-72 flex justify-center flex-col items-center  ">
+        <header id="home" className="w-full h-72 flex justify-center flex-col items-center  ">
           <img
             src="../src/assets/Logo.png"
             alt=""
             className="w-1/5 mb-5 h-5/5 "
           />
           <nav className="flex items-center gap-4 ">
-            <Link href="">Home</Link>
+            <Link href="#home">Home</Link>
 
             <Separator />
-            <Link href="">about me</Link>
+            <Link href="#my-info">about me</Link>
 
             <Separator />
-            <Link href="">projects</Link>
+            <Link href="#projects">projects</Link>
 
             <Separator />
-            <Link href="">contact</Link>
+            <Link href="#contacts">contact</Link>
           </nav>
         </header>
 
         <section className="h-80 flex justify-center w-full">
           <div className=" gap-6 mt-6 flex justify-center w-2/3">
-            <div
+            <img
+              src="../src/assets/cartun.jpeg"
               id="img"
-              className="bg-orange-700 w-36 h-36 rounded-full "
-            ></div>
+              className="-700 w-36 h-36 rounded-full "
+            ></img>
             <div className=" flex-col w-2/6">
               <h1 className="text-orange-600 text-2xl">Hello Word</h1>
               <p className=" text-zinc-400 text-xl  ">
-                I am a Freelancer Fontend and backend developer
+                I am a Fontend and backend developer Freelancer.
               </p>
-              <button className="bg-orange-600 w-2/6 rounded-xl h-10 font-semibold text-zinc-300 mt-3">
-                More
-              </button>
+              <a href="#my-info">
+                <button className="bg-orange-600 w-2/6 rounded-xl h-10 font-semibold text-zinc-300 mt-3">
+                  More
+                </button>
+              </a>
             </div>
           </div>
         </section>
 
-        <section className="h-96 flex gap-6 justify-center ">
-          <div id="img" className="bg-orange-700 w-64 h-96 rounded-2xl "></div>
+        <section id="my-info" className=" h-96 flex gap-6 justify-center ">
+          <div
+            id="img"
+            className="bg-[url('../src/assets/foto.jpeg')] bg-cover  w-64 h-96 rounded-2xl "
+          ></div>
           <div className=" flex-col w-2/6">
             <h1 className="text-orange-600">about me</h1>
             <p className=" text-zinc-400 text-xl  ">
@@ -77,9 +83,13 @@ function App() {
               <div className="flex flex-col">
                 <p className="text-orange-500">Anderson Kawano</p>
                 <p className="text-orange-500">
-                  {dateNow.diff(date, "year")} ago
+                  {dateNow.diff(date, "year")} age
                 </p>
-                <p className="text-orange-500">andersonkawano09@gmail.com</p>
+                <p className="text-orange-500">
+                  <Link href="mailto:andersonkawano09@gmail.com">
+                    andersonkawano09@gmail.com
+                  </Link>
+                </p>
                 <p className="text-orange-500">
                   <Link href="https://github.com/HikaruKawano">
                     github.com/HikaruKawano
@@ -90,23 +100,117 @@ function App() {
           </div>
         </section>
 
-        <section className="h-auto flex gap-6 mt-40 flex-col justify-center  items-center ">
+        <section id="projects" className="h-auto flex gap-6 mt-40 flex-col justify-center  items-center ">
           <h1 className="text-orange-600 text-xl"> Portifolio </h1>
 
           <div className=" flex flex-wrap gap-7 h-full w-5/6 items-center justify-center">
-            <div className="card bg-orange-600 w-64 h-96 rounded-2xl "></div>
+            <div className="card w-96 h-64 rounded-2xl flex items-center justify-center  ">
+              <img
+                className="projects w-full h-full rounded-2xl"
+                src="./src/assets/devFinances.png"
+                alt=""
+              />
+              <h2 className="text-orange-600 font-semibold"> Dev Finances </h2>
+              <p className="text-zinc-500 font-normal text-sm">
+                finance management website
+              </p>
+              <div className="button flex gap-3">
+                <a
+                  href="https://github.com/HikaruKawano/DevFinances-react"
+                  target="_blank"
+                  className="w-auto h-10"
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view code
+                  </button>
+                </a>
 
-            <div className="card bg-orange-600 w-64 h-96 rounded-2xl "></div>
+                <a
+                  href="https://dev-finances-react.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=""
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view site
+                  </button>
+                </a>
+              </div>
+            </div>
 
-            <div className="card bg-orange-600 w-64 h-96 rounded-2xl "></div>
+            <div className="card w-96 h-64 rounded-2xl ">
+              <img
+                className="projects w-full h-full rounded-2xl"
+                src="./src/assets/Cards.png"
+                alt=""
+              />
 
-            <div className="card bg-orange-600 w-64 h-96 rounded-2xl "></div>
+              <h2 className="text-orange-600 font-semibold"> Cards </h2>
+              <p className="text-zinc-500 font-normal text-sm">
+                cards created for science fair
+              </p>
+              <div className="button flex gap-3">
+                <a
+                  href="https://github.com/HikaruKawano/Card"
+                  target="_blank"
+                  className="w-auto h-10"
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view code
+                  </button>
+                </a>
 
-            <div className="card bg-orange-600 w-64 h-96 rounded-2xl "></div>
+                <a
+                  href="https://card-ochre.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=""
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view site
+                  </button>
+                </a>
+              </div>
+            </div>
+
+            <div className="card w-96 h-64 rounded-2xl ">
+              <img
+                className="projects w-full h-full rounded-2xl"
+                src="./src/assets/littleDev.png"
+                alt=""
+              />
+
+              <h2 className="text-orange-600 font-semibold"> LittleDev </h2>
+              <p className="text-zinc-500 font-normal text-sm">
+                website of a fictitious company created for studies
+              </p>
+              <div className="button flex gap-3">
+                <a
+                  href="https://github.com/HikaruKawano/LittleDev"
+                  target="_blank"
+                  className="w-auto h-10"
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view code
+                  </button>
+                </a>
+
+                <a
+                  href="https://little-dev.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className=""
+                >
+                  <button className="bg-orange-600 w-auto px-7 rounded-xl pb-5 pt-1 font-semibold text-zinc-300 mt-3">
+                    view site
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
         </section>
 
-        <footer className="mt-48 flex items-center justify-center flex-col">
+        <footer id="contacts" className="mt-48 flex items-center justify-center flex-col">
           <img
             src="../src/assets/Logo.png"
             alt=""
@@ -114,13 +218,24 @@ function App() {
           />
           <div className="flex gap-4 justify-center items-center">
             <div>
-              <Bs.BsInstagram className="text-zinc-100 text-3xl"></Bs.BsInstagram>
+              <a
+                href="https://www.instagram.com/hikaru_kawano/"
+                target="_blank"
+              >
+                {" "}
+                <Bs.BsInstagram className="text-zinc-100 text-3xl"></Bs.BsInstagram>{" "}
+              </a>
             </div>
             <div>
-              <Ai.AiOutlineMail className="text-zinc-100 text-3xl"></Ai.AiOutlineMail>
+              <a href="mailto:andersonkawano09@gmail.com" target="_blank">
+                {" "}
+                <Ai.AiOutlineMail className="text-zinc-100 text-3xl"></Ai.AiOutlineMail>{" "}
+              </a>
             </div>
             <div>
-              <Bs.BsGithub className="text-zinc-100 text-3xl"></Bs.BsGithub>
+              <a href="https://github.com/HikaruKawano" target="_blank">
+                <Bs.BsGithub className="text-zinc-100 text-3xl"></Bs.BsGithub>{" "}
+              </a>
             </div>
           </div>
         </footer>
